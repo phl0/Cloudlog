c6a8dff584a911f3247621a2f741c6fd244b5492
phl0 <github@florian-wolters.de>
2022-04-19 17:40:48 +0200

    Add lookup function for email

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -853,6 +853,26 @@ class Logbook_model extends CI_Model {
 		return $name;
 	}
 
+	function call_email($callsign) {
+		$this->db->select('COL_CALL, COL_EMAIL, COL_TIME_ON');
+		$this->db->where('COL_CALL', $callsign);
+		$where = "COL_EMAIL != \"\"";
+
+		$this->db->where($where);
+
+		$this->db->order_by("COL_TIME_ON", "desc");
+		$this->db->limit(1);
+		$query = $this->db->get($this->config->item('table_name'));
+		$email = "";
+		if ($query->num_rows() > 0)
+		{
+			$data = $query->row();
+			$email = $data->COL_EMAIL;
+		}
+
+		return $email;
+	}
+
 	function call_iota($callsign) {
 		$this->db->select('COL_CALL, COL_IOTA, COL_TIME_ON');
 		$this->db->where('COL_CALL', $callsign);
