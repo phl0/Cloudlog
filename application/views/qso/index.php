eb744b5780b5118435bdbe8063ab99c13517ca67
phl0 <github@florian-wolters.de>
2023-10-10 13:32:41 +0200

    Add pattern check to prevent posting invalid forms

--- application/views/qso/index.php
+++ application/views/qso/index.php
@@ -59,7 +59,7 @@
 
                 <div class="form-group col-md-6">
                   <label for="start_time"><?php echo lang('general_word_time'); ?></label>
-                  <input type="text" class="form-control form-control-sm input_time" name="start_time" id="start_time" value="<?php if (($this->session->userdata('start_time') != NULL && ((time() - $this->session->userdata('time_stamp')) < 24 * 60 * 60))) { echo $this->session->userdata('start_time'); } else {echo date('H:i'); } ?>" size="7" <?php echo ($_GET['manual'] == 0 ? "disabled" : "");  ?>>
+                  <input type="text" class="form-control form-control-sm input_time" name="start_time" id="start_time" pattern="^\d{2}:\d{2}(:\d{2}){0,1}$" title="Time in HH:MM[:SS] format" value="<?php if (($this->session->userdata('start_time') != NULL && ((time() - $this->session->userdata('time_stamp')) < 24 * 60 * 60))) { echo $this->session->userdata('start_time'); } else {echo date('H:i'); } ?>" size="7" <?php echo ($_GET['manual'] == 0 ? "disabled" : "");  ?>>
                 </div>
 
                 <?php if ( $_GET['manual'] == 0 ) { ?>
